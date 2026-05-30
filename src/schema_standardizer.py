dff2d1f99db847bf02af944f5be32771cb86feea
Juan Sebastian Romero Fernandez <jsromerof>
2026-05-30 07:14:32 -0500

    add __main__ function to schema_standardizer pardo function

--- src/schema_standardizer.py
+++ src/schema_standardizer.py
@@ -1,6 +1,8 @@
 from glom import glom
 import apache_beam as beam
-from glom import glom
+from glom import glom, Spec, T
+import json
+import yaml
 
 # Spec for each vendor item — flattens the nested address into the parent level
 # Reserved keys used as markers in the config
@@ -42,6 +44,8 @@ def standardize_messsage(element: dict, mapping_config: dict) -> dict:
     spec = build_spec(mapping_config[element["topic"]])
     return glom(element, spec)
 
+
+
 class SchemaStandardizer(beam.DoFn):
     
     def __init__(self, mapping_config: dict):
@@ -51,3 +55,82 @@ class SchemaStandardizer(beam.DoFn):
     def process(self, element):
         standardized_message = standardize_messsage(element, mapping_config=self.mapping_config)
         yield standardized_message
+
+
+# --- Example usage ---
+if __name__ == "__main__":
+    source = {
+        "topic" : "leyland",
+        "supplier_id":   1,
+        "supplier_name": "supplier_1",
+        "supplier_data": [
+            {
+                "vendor_id":           424216,
+                "vendor_name":         "JOPP INTERIOR GMBH",
+                "leyland_vendor_code": "H16457",
+                "DAF_vendor_code":     "99123",
+                "creation_date":       "2013-04-08T11:28:43",
+                "last_update_date":    "2019-09-10T13:23:14",
+                "address": {
+                    "street1":     "Am Jopp 1",
+                    "street2":     "Additional Street Info",
+                    "street3":     "More Street Info",
+                    "postal_code": "58566",
+                    "city":        "Kierspe",
+                    "country":     "Germany"
+                }
+            },
+            {
+                "vendor_id":           528242,
+                "vendor_name":         "OHM HANER METALLWERK GMBH CO KG",
+                "leyland_vendor_code": "003554",
+                "DAF_vendor_code":     "87148",
+                "creation_date":       "2014-10-24T13:01:59",
+                "last_update_date":    "2019-09-19T15:41:47",
+                "address": {
+                    "street1":     "Am Jopp 1",
+                    "street2":     "Additional Street Info",
+                    "street3":     "More Street Info",
+                    "postal_code": "58566",
+                    "city":        "Kierspe",
+                    "country":     "Germany"
+                }
+            },
+            {
+                "vendor_id":           528243,
+                "vendor_name":         "PETER GFK SPOL SRO",
+                "leyland_vendor_code": "P17032",
+                "DAF_vendor_code":     "86810",
+                "creation_date":       "2014-10-24T13:08:06",
+                "last_update_date":    "2019-09-19T15:41:47",
+                "address": {
+                    "street1":     "ul. PETER 1",
+                    "street2":     "Additional Street Info",
+                    "street3":     "More Street Info",
+                    "postal_code": "58566",
+                    "city":        "Kierspe",
+                    "country":     "Germany"
+                }
+            }
+        ]
+    }
+    
+    
+    def load_mapping_dict(config_path: str, mapping_name: str) -> dict:
+        mapping_config = {}
+        with open(config_path, "r") as file:
+            config = yaml.safe_load(file)
+            
+            for item in config.get("topics", []):
+                mapping_config[item["topic"]] = item["standardization_mapping"]
+
+        if mapping_name not in config:
+            raise KeyError(f"Mapping '{mapping_name}' not found in {config_path}")
+        
+        return mapping_config
+    
+    mapping_config = load_mapping_dict(config_path="local.yaml", mapping_name="topics")
+    
+    print(mapping_config)
+    result = standardize_messsage(source, mapping_config)
+    print(json.dumps(result, indent=4))
